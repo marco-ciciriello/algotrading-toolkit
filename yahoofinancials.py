41b2ebd54d5a1f3540340728ed1cdaedd8976461
Marco Ciciriello <68699414+marco-ciciriello@users.noreply.github.com>
2020-12-29 10:41:31 +0000

    feat: add yahoofinancials closing price data

---
+++ yahoofinancials.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import pandas as pd
+
+from yahoofinancials import YahooFinancials
+
+tickers = ['AAPL', 'MSFT', 'CSCO', 'AMZN', 'INTC']
+
+closing_prices = pd.DataFrame()
+end_date = (dt.date.today()).strftime('%Y-%m-%d')
+start_date = (dt.date.today() - dt.timedelta(253)).strftime('%Y-%m-%d')  # One trading year = 253 days
+
+# Extract historical close price for all stocks identified
+for ticker in tickers:
+    yahoofinancials = YahooFinancials(ticker)
+    json_return = yahoofinancials.get_historical_price_data(start_date, end_date, 'daily')
+    ohlcv = json_return[ticker]['prices']
+    temp_df = pd.DataFrame(ohlcv)[['formatted_date', 'adjclose']]
+    temp_df.set_index('formatted_date', inplace=True)
+    temp_df.dropna(inplace=True)
+    closing_prices[ticker] = temp_df['adjclose']
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+print(closing_prices)
