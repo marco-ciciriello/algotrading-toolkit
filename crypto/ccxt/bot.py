82cd9ef43e01808535b987bb8f78301f9f648ef9
Marco Ciciriello <68699414+marco-ciciriello@users.noreply.github.com>
2021-04-08 14:39:46 +0100

    feat: add atr and bollinger band technical analysis

--- crypto/ccxt/bot.py
+++ crypto/ccxt/bot.py
@@ -1,14 +1,26 @@
 import ccxt
 import config
+import pandas as pd
+import ta
+
+from ta.volatility import AverageTrueRange, BollingerBands
 
 exchange = ccxt.binance({
     'apiKey': config.BINANCE_API_KEY,
-    'secret': config.BINANCE_API_SECRET,
-    'timeout': 30000,
-    'enableRateLimit': True,
+    'secret': config.BINANCE_API_SECRET
 })
 
-balance = exchange.fetch_balance()
+markets = exchange.load_markets()
+bars = exchange.fetch_ohlcv('ETH/USDT', limit=20)
 
-order = exchange.create_market_buy_order('ETH/USD', 0.01)
-print(order)
+df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+
+bb_indicator = BollingerBands(df['close'])
+df['upper_band'] = bb_indicator.bollinger_hband()
+df['lower_band'] = bb_indicator.bollinger_lband()
+df['moving_average'] = bb_indicator.bollinger_mavg()
+
+atr_indicator = AverageTrueRange(df['high'], df['low'], df['close'])
+df['atr'] = atr_indicator.average_true_range()
+
+print(df)
